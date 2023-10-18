f2d3dedd9cdb8f1d5d88084f94b5858621bd63d9
Zachary Toews <zachary.peter.toews@gmail.com>
2023-10-18 18:50:38 +1300

    Rook and Bishop no longer move through pieces

--- game/Bishop.java
+++ game/Bishop.java
@@ -12,7 +12,7 @@ public class Bishop extends Piece {
 
     final String BBISHOP_IMAGE = "resources/game/BBishop.png";//imports for the images
     ImageIcon BB_IMAGE_ICON = new ImageIcon(BBISHOP_IMAGE);//imports for the images
-    
+
     public static final String BISHOP = "Bishop";
     //Aspects of the bishop
     public Bishop(boolean black, boolean firstMove) {
@@ -29,9 +29,36 @@ public class Bishop extends Piece {
         }
     }
 
+    //This methhot dictates what the valid move are for this piece
     @Override
-    public boolean validMove() {
-        //This methhot dictates what the valid move are for this piece
-        return Math.abs(ChessBoard.changeInRank) == Math.abs(ChessBoard.changeInFile);
+public boolean validMove() {
+    //int changeInRank = Math.abs(ChessBoard.changeInRank);
+    //int changeInFile = Math.abs(ChessBoard.changeInFile);
+    // Check if the bishop is moving diagonally
+    if (Math.abs(ChessBoard.changeInRank) == Math.abs(ChessBoard.changeInFile)) {
+        System.out.println("Start of valid move");
+        int rankDirection = (ChessBoard.changeInRank > 0) ? 1 : -1;
+        int fileDirection = (ChessBoard.changeInFile > 0) ? 1 : -1;
+        for (int i = 1; i < Math.abs(ChessBoard.changeInRank); i++) {
+            int rankToCheck = ChessBoard.rankClickedOn + i * rankDirection;
+            int fileToCheck = ChessBoard.fileClickedOn + i * fileDirection;
+            System.out.println("Started the for loop");
+            if (ChessBoard.getPieceAtFileRank(fileToCheck, rankToCheck) != null) {
+                ChessBoard.pieceInTheWay = true;
+                System.out.println("Found a piece");
+                break; // A piece is in the way, stop checking
+            } else {
+                ChessBoard.pieceInTheWay = false;
+            }
+        }
+        //return !ChessBoard.pieceInTheWay;
     }
+    if(ChessBoard.pieceInTheWay == false){            
+            return Math.abs(ChessBoard.changeInRank) == Math.abs(ChessBoard.changeInFile);
+        }else{            
+            return false;
+        }
+    //return false; // Not a valid diagonal move
+}
+   
 }
--- game/Rook.java
+++ game/Rook.java
@@ -48,136 +48,46 @@ public class Rook extends Piece {
             }else{
                 directionFile = -1;
             }
-        }           
-        //ChessBoard.startingFile = ChessBoard.changeInFile - ChessBoard.fileClickedOn;
-        //ChessBoard.startingRank = ChessBoard.changeInRank - ChessBoard.rankClickedOn;
-        System.out.println("change in file " + ChessBoard.changeInFile + "");
-        System.out.println("change in rank " + ChessBoard.changeInRank + "");
-        System.out.println("file clicked on " + ChessBoard.fileClickedOn + "");
-        System.out.println("rank clicked on " + ChessBoard.rankClickedOn + "");
-        //System.out.println("starting file " + ChessBoard.startingFile + "");
-        //System.out.println("starting rank " + ChessBoard.startingRank + "");
-        if(ChessBoard.changeInRank > 0 && ChessBoard.changeInFile == 0){//Walking down the ranks looking for pieces
-            /*
-            System.out.println("rank clicked in the if is " + ChessBoard.rankClickedOn + "");
-            System.out.println("change in rank in the if is " + ChessBoard.changeInRank + "");
-            System.out.println("change in rank + rank clicked on in the if is " + ChessBoard.rankClickedOn + ChessBoard.changeInRank + "");
-            System.out.println("---------Direction rank is " + directionRank +"");
-            */
-            for(int rRank = ChessBoard.rankClickedOn + 1; rRank != (ChessBoard.rankClickedOn + ChessBoard.changeInRank); rRank += directionRank){
-                /*
-                System.out.println("++++++++++++++++++=change in rank at start is " + ChessBoard.changeInRank + "");
-                System.out.println("rank clicked on + change in rank ============= " + (ChessBoard.rankClickedOn + ChessBoard.changeInRank) + ""); 
-                System.out.println("==========rRank is " + rRank + "");            
-                System.out.println("rank clicked on " + ChessBoard.rankClickedOn + "");
-                */
-                
-                //System.out.println("================change in rank at the middle 5 is " + ChessBoard.changeInRank + "");  
+        }                   
+        if(ChessBoard.changeInRank > 0 && ChessBoard.changeInFile == 0){//Walking down the ranks looking for pieces           
+            for(int rRank = ChessBoard.rankClickedOn + 1; rRank != (ChessBoard.rankClickedOn + ChessBoard.changeInRank); rRank += directionRank){             
                 if(ChessBoard.getPieceAtFileRank(ChessBoard.fileClickedOn,rRank) != null){
-                    //System.out.println("================change in rank at mid 7 is " + ChessBoard.changeInRank + "");  
                     ChessBoard.pieceInTheWay = true;
-                    //rRank = ChessBoard.rankClickedOn + ChessBoard.changeInRank;
                 }else{
-                    //System.out.println("================change in rank at mid 7a is " + ChessBoard.changeInRank + "");  
-                    System.out.println("rank " + rRank + " is null");
-                    //ChessBoard.pieceInTheWay = false;
-                }
-                //System.out.println("================change in rank at end is " + ChessBoard.changeInRank + "");            
+                    ChessBoard.pieceInTheWay = false;
+                }          
             }
         }else if(ChessBoard.changeInRank < 0 && ChessBoard.changeInFile == 0){
-            for(int rRank = ChessBoard.rankClickedOn -1; rRank != (ChessBoard.rankClickedOn + ChessBoard.changeInRank); rRank += directionRank){
-                /*
-                System.out.println("++++++++++++++++++=change in rank at start is " + ChessBoard.changeInRank + "");
-                System.out.println("rank clicked on + change in rank ============= " + (ChessBoard.rankClickedOn + ChessBoard.changeInRank) + ""); 
-                System.out.println("==========rRank is " + rRank + "");            
-                System.out.println("rank clicked on " + ChessBoard.rankClickedOn + "");
-                */
-                
-                //System.out.println("================change in rank at the middle 5 is " + ChessBoard.changeInRank + "");  
+            for(int rRank = ChessBoard.rankClickedOn -1; rRank != (ChessBoard.rankClickedOn + ChessBoard.changeInRank); rRank += directionRank){                
                 if(ChessBoard.getPieceAtFileRank(ChessBoard.fileClickedOn,rRank) != null){
-                    //System.out.println("================change in rank at mid 7 is " + ChessBoard.changeInRank + "");  
                     ChessBoard.pieceInTheWay = true;
-                    //rRank = ChessBoard.rankClickedOn + ChessBoard.changeInRank;
                 }else{
-                    //System.out.println("================change in rank at mid 7a is " + ChessBoard.changeInRank + "");  
-                    System.out.println("rank " + rRank + " is null");
-                    //ChessBoard.pieceInTheWay = false;
-                }
-                //System.out.println("================change in rank at end is " + ChessBoard.changeInRank + "");            
+                    ChessBoard.pieceInTheWay = false;
+                }        
             }
         }else if(ChessBoard.changeInFile > 0 && ChessBoard.changeInRank == 0){
             for(int rFile = ChessBoard.fileClickedOn +1; rFile != (ChessBoard.fileClickedOn + ChessBoard.changeInFile); rFile += directionFile){
-                /*
-                System.out.println("++++++++++++++++++=change in rank at start is " + ChessBoard.changeInRank + "");
-                System.out.println("rank clicked on + change in rank ============= " + (ChessBoard.rankClickedOn + ChessBoard.changeInRank) + ""); 
-                System.out.println("==========rRank is " + rRank + "");            
-                System.out.println("rank clicked on " + ChessBoard.rankClickedOn + "");
-                */
-                
-                //System.out.println("================change in rank at the middle 5 is " + ChessBoard.changeInRank + "");  
                 if(ChessBoard.getPieceAtFileRank(rFile,ChessBoard.rankClickedOn) != null){
-                    //System.out.println("================change in rank at mid 7 is " + ChessBoard.changeInRank + "");  
                     ChessBoard.pieceInTheWay = true;
-                    //rRank = ChessBoard.rankClickedOn + ChessBoard.changeInRank;
-                }else{
-                    //System.out.println("================change in rank at mid 7a is " + ChessBoard.changeInRank + "");  
-                    System.out.println("rank " + rFile + " is null");
-                    //ChessBoard.pieceInTheWay = false;
+                }else{                    
+                    ChessBoard.pieceInTheWay = false;
                 }
-                //System.out.println("================change in rank at end is " + ChessBoard.changeInRank + "");            
             }
         }else if(ChessBoard.changeInFile < 0 && ChessBoard.changeInRank == 0){
-            for(int rFile = ChessBoard.fileClickedOn -1; rFile != (ChessBoard.fileClickedOn + ChessBoard.changeInFile); rFile += directionFile){
-                /*
-                System.out.println("++++++++++++++++++=change in rank at start is " + ChessBoard.changeInRank + "");
-                System.out.println("rank clicked on + change in rank ============= " + (ChessBoard.rankClickedOn + ChessBoard.changeInRank) + ""); 
-                System.out.println("==========rRank is " + rRank + "");            
-                System.out.println("rank clicked on " + ChessBoard.rankClickedOn + "");
-                */
-                
-                //System.out.println("================change in rank at the middle 5 is " + ChessBoard.changeInRank + "");  
-                if(ChessBoard.getPieceAtFileRank(rFile,ChessBoard.rankClickedOn) != null){
-                    //System.out.println("================change in rank at mid 7 is " + ChessBoard.changeInRank + "");  
+            for(int rFile = ChessBoard.fileClickedOn -1; rFile != (ChessBoard.fileClickedOn + ChessBoard.changeInFile); rFile += directionFile){                
+                if(ChessBoard.getPieceAtFileRank(rFile,ChessBoard.rankClickedOn) != null){                      
                     ChessBoard.pieceInTheWay = true;
-                    //rRank = ChessBoard.rankClickedOn + ChessBoard.changeInRank;
                 }else{
-                    //System.out.println("================change in rank at mid 7a is " + ChessBoard.changeInRank + "");  
-                    System.out.println("rank " + rFile + " is null");
-                    //ChessBoard.pieceInTheWay = false;
-                }
-                //System.out.println("================change in rank at end is " + ChessBoard.changeInRank + "");            
+                    ChessBoard.pieceInTheWay = false;
+                }    
             }
         }else{
             System.out.println("Not a valid rook move");
         }
-        /*
-        if(ChessBoard.changeInFile < 0){//Walking down the files looking for pieces
-        for(int rRank = ChessBoard.rankClickedOn; rRank != ChessBoard.rankClickedOn; rRank+=directionRank){
-        if(ChessBoard.getPiece(ChessBoard.fileClickedOn,rRank) != null){
-        ChessBoard.pieceInTheWay = true;
-        }else{
-        ChessBoard.pieceInTheWay = false;
-        }
-        }
-        }else{
-        for(int rFile = ChessBoard.fileClickedOn; rFile != ChessBoard.fileClickedOn; rFile+=directionFile){
-        if(ChessBoard.getPiece(rFile,ChessBoard.rankClickedOn) != null){
-        ChessBoard.pieceInTheWay = true;
-        }else{
-        ChessBoard.pieceInTheWay = false;
-        }
-        }
-        } 
-         */
-        if(ChessBoard.pieceInTheWay == false){
-            //System.out.println("Third file " + ChessBoard.startingFile + "");
-            //System.out.println("Third rank " + ChessBoard.startingRank + "");
+        if(ChessBoard.pieceInTheWay == false){            
             return ChessBoard.changeInRank == ChessBoard.NO_CHANGE_IN_RANK || 
             ChessBoard.changeInFile == ChessBoard.NO_CHANGE_IN_FILE;
-        }else{
-            //System.out.println("Third " + ChessBoard.startingFile + "");
-            //System.out.println("Third " + ChessBoard.startingRank + "");
-
+        }else{            
             return false;
         }
     }
